72cb9c0f6b8a6a900f67262dcc69ff3308cc6d0f
AlexandrePellerin <pellerin.alex@orange.fr>
2016-12-16 15:56:31 +0100

    on a pas travailler

--- TP7/src/Main.java
+++ TP7/src/Main.java
@@ -1,10 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
 
+	private final static String KEY = "MzJPF68KYbwEXaqFr3rasHfabgXU5dcd";
+
 	public static void main(String[] args) throws Exception {
 
-		String key = "MzJPF68KYbwEXaqFr3rasHfabgXU5dcd";
 		String champ[] = null;
 		String distance = null;
 		String depart[] = new String[4];
@@ -13,6 +17,12 @@ public class Main {
 		System.setProperty("http.proxyHost", "cache.univ-lille1.fr");
 		System.setProperty("http.proxyPort", "3128");
 
-		URL u = new URL("http://open.mapquestapi.com/directions/v2/route?key=KEY&from=Lille&to=Paris");
+		URL u = new URL("http://open.mapquestapi.com/directions/v2/route?key=" + KEY + "&from=Lille&to=Paris");
+		URLConnection c = u.openConnection();
+		BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			System.out.println(inputLine);
+		in.close();
 	}
 }
